97cb83f2eee1d6d187246ce5385f9c0793d54331
gustavohnsv <guvi10vieira@gmail.com>
2024-04-30 21:09:04 -0300

    feat: added new routes for methods

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } f
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
 import { ListCustomersController } from "./controllers/ListCustomersController";
 import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
+import { UpdateCustomerController } from "./controllers/UpdateCustomerController";
+import { UpdateSttsCustomerController } from "./controllers/UpdateSttsCustomerController";
+import { request } from "http";
 
 export async function routes(fastify: FastifyInstance, option: FastifyPluginOptions) {
 
@@ -25,6 +28,18 @@ export async function routes(fastify: FastifyInstance, option: FastifyPluginOpti
     // Rota para deletar um cliente
     fastify.delete("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
         return new DeleteCustomerController().handle(request, reply);
+    });
+
+    // Rota UPDATE-CUSTOMER
+    // Rota para atualizar um cliente
+    fastify.post("/updatecustomer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new UpdateCustomerController().handle(request, reply);
+    });
+
+    // Rota UPDATE-STATUS-CUSTOMER
+    // Rota para atualizar o status de um cliente
+    fastify.post("/updatesttscustomer", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new UpdateSttsCustomerController().handle(request, reply);
     });
 
 }
